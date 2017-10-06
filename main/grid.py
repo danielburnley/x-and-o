96bce333368c079a0aa2b78bcca1bf598e9a5948
Daniel Burnley <dan@danops.uk>
2017-10-06 18:01:18 +0100

    add spaces to grid

--- main/grid.py
+++ main/grid.py
@@ -3,11 +3,11 @@ class IllegalMoveError(Exception):
 
 class Grid():
   def __init__(self):
-    self.grid = [[ "" for x in range(3)] for x in range(3)]
+    self.grid = [[ " " for x in range(3)] for x in range(3)]
     self.turns = 0
 
   def set_cell(self, val, pos):
-    if self.grid[pos[0]][pos[1]] != "":
+    if self.grid[pos[0]][pos[1]] != " ":
       raise IllegalMoveError
     self.grid[pos[0]][pos[1]] = val
     self.turns = self.turns + 1
@@ -16,13 +16,13 @@ class Grid():
     return self._row_won() or self._col_won() or self._diagonals_won()
 
   def has_draw(self):
-    return "" not in self._flattened_grid() and not self.has_won()
+    return " " not in self._flattened_grid() and not self.has_won()
 
   def possible_moves(self):
     moves = []
     for index, row in enumerate(self.grid):
       for cell_index, cell in enumerate(row):
-        if cell == "":
+        if cell == " ":
           moves.append((index, cell_index))
     return moves
 
@@ -53,7 +53,7 @@ class Grid():
     return False
 
   def _all_elements_equal_and_not_empty(self, arr):
-    return "" not in arr and len(set(arr)) == 1
+    return " " not in arr and len(set(arr)) == 1
 
   def _flattened_grid(self):
     return [y for x in self.grid for y in x]
--- tests/grid_test.py
+++ tests/grid_test.py
@@ -3,109 +3,109 @@ import pytest
 from main.grid import Grid, IllegalMoveError
 
 class TestGrid:
-  WINNING_TEST_CASES = [
-    [["X","X","X"],["","",""],["","",""]],
-    [["","",""],["X","X","X"],["","",""]],
-    [["","",""],["","",""],["X","X","X"]],
-    [["X","",""],["X","",""],["X","",""]],
-    [["","X",""],["","X",""],["","X",""]],
-    [["","","X"],["","","X"],["","","X"]],
-    [["X","",""],["","X",""],["","","X"]],
-    [["","","X"],["","X",""],["X","",""]],
-    [["O","O","O"],["","",""],["","",""]],
-    [["","",""],["O","O","O"],["","",""]],
-    [["","",""],["","",""],["O","O","O"]],
-    [["O","",""],["O","",""],["O","",""]],
-    [["","O",""],["","O",""],["","O",""]],
-    [["","","O"],["","","O"],["","","O"]],
-    [["O","",""],["","O",""],["","","O"]],
-    [["","","O"],["","O",""],["O","",""]],
-  ]
+    WINNING_TEST_CASES = [
+        [["X", "X", "X"], [" ", " ", " "], [" ", " ", " "]],
+        [[" ", " ", " "], ["X", "X", "X"], [" ", " ", " "]],
+        [[" ", " ", " "], [" ", " ", " "], ["X", "X", "X"]],
+        [["X", " ", " "], ["X", " ", " "], ["X", " ", " "]],
+        [[" ", "X", " "], [" ", "X", " "], [" ", "X", " "]],
+        [[" ", " ", "X"], [" ", " ", "X"], [" ", " ", "X"]],
+        [["X", " ", " "], [" ", "X", " "], [" ", " ", "X"]],
+        [[" ", " ", "X"], [" ", "X", " "], ["X", " ", " "]],
+        [["O", "O", "O"], [" ", " ", " "], [" ", " ", " "]],
+        [[" ", " ", " "], ["O", "O", "O"], [" ", " ", " "]],
+        [[" ", " ", " "], [" ", " ", " "], ["O", "O", "O"]],
+        [["O", " ", " "], ["O", " ", " "], ["O", " ", " "]],
+        [[" ", "O", " "], [" ", "O", " "], [" ", "O", " "]],
+        [[" ", " ", "O"], [" ", " ", "O"], [" ", " ", "O"]],
+        [["O", " ", " "], [" ", "O", " "], [" ", " ", "O"]],
+        [[" ", " ", "O"], [" ", "O", " "], ["O", " ", " "]],
+    ]
 
-  def test_starting_grid(self):
-    assert(Grid().grid == [["","",""],["","",""],["","",""]])
+    def test_starting_grid(self):
+        assert Grid().grid == [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
 
-  def test_setting_the_top_left_square_to_X(self):
-    grid = Grid()
-    grid.set_cell("X", (0, 0))
+    def test_setting_the_top_left_square_to_X(self):
+        grid = Grid()
+        grid.set_cell("X", (0, 0))
 
-    assert(grid.grid[0][0] == "X")
-    assert(grid.turns == 1)
+        assert grid.grid[0][0] == "X"
+        assert grid.turns == 1
 
-  def test_setting_the_middle_square(self):
-    grid = Grid()
-    grid.set_cell("O", (1, 1))
+    def test_setting_the_middle_square(self):
+        grid = Grid()
+        grid.set_cell("O", (1, 1))
 
-    assert(grid.grid[1][1] == "O")
-    assert(grid.turns == 1)
+        assert grid.grid[1][1] == "O"
+        assert grid.turns == 1
 
-  def test_setting_multiple_cells(self):
-    grid = Grid()
-    grid.set_cell("X", (0, 0))
-    grid.set_cell("X", (0, 1))
+    def test_setting_multiple_cells(self):
+        grid = Grid()
+        grid.set_cell("X", (0, 0))
+        grid.set_cell("X", (0, 1))
 
-    assert(grid.turns == 2)
+        assert grid.turns == 2
 
-  def test_has_won_with_empty_grid(self):
-    assert(not Grid().has_won())
+    def test_has_won_with_empty_grid(self):
+        assert not Grid().has_won()
 
-  def test_has_won_with_top_row_mix_of_X_and_O(self):
-    grid = Grid()
-    grid.set_cell("X", (0, 0))
-    grid.set_cell("O", (0, 1))
-    grid.set_cell("X", (0, 2))
+    def test_has_won_with_top_row_mix_of_X_and_O(self):
+        grid = Grid()
+        grid.set_cell("X", (0, 0))
+        grid.set_cell("O", (0, 1))
+        grid.set_cell("X", (0, 2))
 
-    assert(not grid.has_won())
+        assert not grid.has_won()
 
-  def test_has_won_with_left_row_mix_of_X_and_O(self):
-    grid = Grid()
-    grid.set_cell("X", (0, 0))
-    grid.set_cell("O", (1, 0))
-    grid.set_cell("X", (2, 0))
+    def test_has_won_with_left_row_mix_of_X_and_O(self):
+        grid = Grid()
+        grid.set_cell("X", (0, 0))
+        grid.set_cell("O", (1, 0))
+        grid.set_cell("X", (2, 0))
 
-    assert(not grid.has_won())
+        assert not grid.has_won()
 
-  def test_win_conditions(self):
-    grid = Grid()
-    for tested_grid in self.WINNING_TEST_CASES:
-      grid.grid = tested_grid
-      assert(grid.has_won())
+    def test_win_conditions(self):
+        grid = Grid()
+        for tested_grid in self.WINNING_TEST_CASES:
+            grid.grid = tested_grid
+            assert grid.has_won()
 
-  def test_has_draw_with_empty_grid(self):
-    assert(not Grid().has_draw())
+    def test_has_draw_with_empty_grid(self):
+        assert not Grid().has_draw()
 
-  def test_has_draw_with_empty_grid(self):
-    grid = Grid()
-    grid.grid = [["X", "O", "X"], ["O", "X", "O"], ["O", "X", "O"]]
-    assert(grid.has_draw())
+    def test_has_draw_with_empty_grid(self):
+        grid = Grid()
+        grid.grid = [["X", "O", "X"], ["O", "X", "O"], ["O", "X", "O"]]
+        assert grid.has_draw()
 
-  def test_has_draw_with_winning_row(self):
-    grid = Grid()
-    grid.grid = [["X", "X", "O"], ["X", "O", "O"], ["X", "O", "X"]]
+    def test_has_draw_with_winning_row(self):
+        grid = Grid()
+        grid.grid = [["X", "X", "O"], ["X", "O", "O"], ["X", "O", "X"]]
 
-    assert(not grid.has_draw())
+        assert not grid.has_draw()
 
 
-  def test_raises_exception_for_overriding_moves(self):
-    grid = Grid()
-    grid.set_cell('X', (0, 0))
-    with pytest.raises(IllegalMoveError):
-          grid.set_cell('X', (0, 0))
+    def test_raises_exception_for_overriding_moves(self):
+        grid = Grid()
+        grid.set_cell('X', (0, 0))
+        with pytest.raises(IllegalMoveError):
+            grid.set_cell('X', (0, 0))
 
-  # All possible moves
+# All possible moves
 
-  def test_one_move_remaining(self):
-    grid = Grid()
-    grid.grid = [["X", "O", "X"], ["O", "X", "O"], ["O", "X", ""]]
+    def test_one_move_remaining(self):
+        grid = Grid()
+        grid.grid = [["X", "O", "X"], ["O", "X", "O"], ["O", "X", " "]]
 
-    possible_moves = grid.possible_moves()
+        possible_moves = grid.possible_moves()
 
-    assert(possible_moves == [(2,2)])
+        assert possible_moves == [(2,2)]
 
-  def test_multiple_moves_remaining(self):
-    grid = Grid()
-    grid.grid = [["", "O", "X"], ["O", "", "O"], ["O", "X", ""]]
+    def test_multiple_moves_remaining(self):
+        grid = Grid()
+        grid.grid = [[" ", "O", "X"], ["O", " ", "O"], ["O", "X", " "]]
 
-    possible_moves = grid.possible_moves()
+        possible_moves = grid.possible_moves()
 
-    assert(possible_moves == [(0,0),(1,1),(2,2)])
+        assert possible_moves == [(0,0),(1,1),(2,2)]

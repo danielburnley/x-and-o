1e7900af97708932ed2355de1116f89588448ba8
Daniel Burnley <dan@danops.uk>
2017-10-06 18:00:34 +0100

    remove broken AI stuff

--- main/ai.py
+++ main/ai.py
@@ -1,8 +1,9 @@
 from main.grid import Grid
 
 class AI:
-  def next_move(self, grid: Grid):
-    if grid.turns == 1:
-      if grid.grid[0][0] == "X":
-        return (1, 1)
-    return grid.possible_moves()[0]
+  pass
+  # def next_move(self, grid: Grid):
+  #   if grid.turns == 1:
+  #     if grid.grid[0][0] == "X":
+  #       return (1, 1)
+  #   return grid.possible_moves()[0]
--- tests/ai_test.py
+++ tests/ai_test.py
@@ -2,23 +2,24 @@ from main.ai import AI
 from main.grid import Grid
 
 class TestAI:
-  def test_only_one_move_possible(self):
-    grid = Grid()
-    ai = AI()
+  pass
+  # def test_only_one_move_possible(self):
+  #   grid = Grid()
+  #   ai = AI()
 
-    grid.grid = [["X", "O", "X"], ["O", "X", "O"], ["O", "X", ""]]
-    assert(ai.next_move(grid) == (2,2))
+  #   grid.grid = [["X", "O", "X"], ["O", "X", "O"], ["O", "X", ""]]
+  #   assert(ai.next_move(grid) == (2,2))
 
-    grid.grid = [["X", "", "X"], ["O", "X", "O"], ["O", "X", "O"]]
-    assert(ai.next_move(grid) == (0,1))
+  #   grid.grid = [["X", "", "X"], ["O", "X", "O"], ["O", "X", "O"]]
+  #   assert(ai.next_move(grid) == (0,1))
 
-  def test_all_moves_possible(self):
-    assert(AI().next_move(Grid()) == (0,0))
+  # def test_all_moves_possible(self):
+  #   assert(AI().next_move(Grid()) == (0,0))
 
-  def test_other_player_started_not_in_center(self):
-    grid = Grid()
-    ai = AI()
+  # def test_other_player_started_not_in_center(self):
+  #   grid = Grid()
+  #   ai = AI()
 
-    grid.set_cell("X", (0, 0))
-    assert(ai.next_move(grid) == (1,1))
+  #   grid.set_cell("X", (0, 0))
+  #   assert(ai.next_move(grid) == (1,1))
 

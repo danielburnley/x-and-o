7012942c5a1bab71858a0d40003e64b188f7bf4f
Daniel Burnley <dan@danops.uk>
2017-10-27 14:24:27 +0100

    refactor ai test

--- tests/ai_test.py
+++ tests/ai_test.py
@@ -3,100 +3,89 @@ from main.game import Game
 from main.grid import Grid
 
 class TestAI:
-    def test_score_when_AI_has_not_won(self):
+    def get_new_game(self, grid, player='X'):
         game = Game(Grid())
-        game.grid.grid = [["O","O","O"],[" ", " ", " "],[" ", " ", " "]]
-        ai = AI(game=game, player="X")
-        assert ai.score(game) == -10
+        game.grid.grid = grid
+        game.player = player
+        return game
+
+    def get_ai_for_game(self, game, player):
+        return AI(game, player)
+
+    def assert_score(self, ai, game, expected_score):
+        assert ai.score(game) == expected_score
+
+    def assert_number_of_possible_moves(self, ai, game, expected_number_of_moves):
+        assert len(ai.get_possible_moves(game)) == expected_number_of_moves
+
+    def assert_minmax_score(self, ai, game, expected_score):
+        assert ai.minmax(game) == expected_score
+
+    def assert_next_move(self, ai, expected_move):
+        assert ai.get_next_move() == expected_move
+
+    def test_score_when_AI_has_not_won(self):
+        game = self.get_new_game([["O","O","O"],[" ", " ", " "],[" ", " ", " "]])
+        ai = self.get_ai_for_game(game, "X")
+        self.assert_score(ai, game, -10)
 
     def test_score_when_AI_has_won(self):
-        game = Game(Grid())
-        game.grid.grid = [["O","O","O"],[" ", " ", " "],[" ", " ", " "]]
-        ai = AI(game=game, player="O")
-        assert ai.score(game) == 10
+        game = self.get_new_game([["O","O","O"],[" ", " ", " "],[" ", " ", " "]])
+        ai = self.get_ai_for_game(game, "O")
+        self.assert_score(ai, game, 10)
 
     def test_score_when_everyone_lose(self):
-        game = Game(Grid())
-        game.grid.grid = [["O","X","O"],[" ", " ", " "],[" ", " ", " "]]
-        ai = AI(game=game, player="O")
-        assert ai.score(game) == 0
+        game = self.get_new_game([["O","X","O"],[" ", " ", " "],[" ", " ", " "]])
+        ai = self.get_ai_for_game(game, "O")
+        self.assert_score(ai, game, 0)
 
     def test_get_possible_moves_when_only_one_move(self):
-        game = Game(Grid())
-        game.grid.grid = [["O","X","O"],["X", " ", "X"],["X", "O", "X"]]
-        game.player = "O"
-        ai = AI(game=game, player="O")
-        assert len(ai.get_possible_moves(game)) == 1
+        game = self.get_new_game([["O","X","O"],["X", " ", "X"],["X", "O", "X"]], 'O')
+        ai = self.get_ai_for_game(game, "O")
+        self.assert_number_of_possible_moves(ai, game, 1)
         assert ai.get_possible_moves(game)[0].grid.grid == [["O", "X", "O"], ["X", "O", "X"], ["X", "O", "X"]]
 
     def test_get_possible_moves_when_only_one_move(self):
-        game = Game(Grid())
-        game.grid.grid = [["O"," ","O"],["X", " ", "X"],["X", " ", "X"]]
-        game.player = "O"
-        ai = AI(game=game, player="O")
-        assert len(ai.get_possible_moves(game)) == 3
+        game = self.get_new_game([["O"," ","O"],["X", " ", "X"],["X", " ", "X"]], "O")
+        ai = self.get_ai_for_game(game, "O")
+        self.assert_number_of_possible_moves(ai, game, 3)
 
     def test_get_possible_moves_when_only_one_move(self):
-        game = Game(Grid())
-        game.grid.grid = [["O","X","O"],["X", "O", "X"],["X", "O", "X"]]
-        game.player = "O"
-        ai = AI(game=game, player="O")
-        assert len(ai.get_possible_moves(game)) == 0
+        game = self.get_new_game([["O","X","O"],["X", "O", "X"],["X", "O", "X"]], "O")
+        ai = self.get_ai_for_game(game, "O")
+        self.assert_number_of_possible_moves(ai, game, 0)
 
     def test_returns_score_for_draw(self):
-        game = Game(Grid())
-        game.grid.grid = [["O","X","O"],["X", "O", "X"],["X", "O", "X"]]
-        game.player = "O"
-        ai = AI(game=game, player="O")
-        assert ai.minmax(game) == 0
+        game = self.get_new_game([["O","X","O"],["X", "O", "X"],["X", "O", "X"]], "O")
+        ai = self.get_ai_for_game(game, "O")
+        self.assert_minmax_score(ai, game, 0)
 
     def test_returns_score_for_AI_won(self):
-        game = Game(Grid())
-        game.grid.grid = [["O","O","O"],["X", "O", "X"],["X", "O", "X"]]
-        game.player = "X"
-        ai = AI(game=game, player="O")
-        assert ai.minmax(game) == 10
+        game = self.get_new_game([["O","O","O"],["X", "O", "X"],["X", "O", "X"]])
+        ai = self.get_ai_for_game(game, "O")
+        self.assert_minmax_score(ai, game, 10)
 
     def test_returns_score_for_AI_lose(self):
-        game = Game(Grid())
-        game.grid.grid = [["O","X","O"],["X", "X", "X"],["X", "X", "X"]]
-        game.player = "O"
-        ai = AI(game=game, player="O")
-        assert ai.minmax(game) == -10
+        game = self.get_new_game([["O","X","O"],["X", "X", "X"],["X", "X", "X"]], "O")
+        ai = self.get_ai_for_game(game, "O")
+        self.assert_minmax_score(ai, game, -10)
 
     def test_next_move(self):
-        game = Game(Grid())
-        game.grid.grid = [["O","X","O"],["X", " ", "X"],["X", "O", "X"]]
-        game.player = "O"
-        ai = AI(game=game, player="O")
-        assert ai.get_next_move() == (1, 1)
+        game = self.get_new_game([["O","X","O"],["X", " ", "X"],["X", "O", "X"]], "O")
+        ai = self.get_ai_for_game(game, "O")
+        self.assert_next_move(ai, (1, 1))
 
     def test_next_move_with_one_losing_move(self):
-        game = Game(Grid())
-        game.grid.grid = [
-            [" ","X","O"],
-            ["X","X","O"],
-            ["X","O"," "]]
-        game.player = "O"
-        ai = AI(game=game, player="O")
-        assert ai.get_next_move() == (2, 2)
+        game = self.get_new_game([[" ","X","O"],["X","X","O"],["X","O"," "]], "O")
+        ai = self.get_ai_for_game(game, "O")
+        self.assert_next_move(ai, (2, 2))
 
     def test_given_first_go_with_player_in_center(self):
-        game = Game(Grid())
-        game.grid.grid = [
-            [" "," "," "],
-            [" ","X"," "],
-            [" "," "," "]]
-        game.player = "O"
-        ai = AI(game=game, player="O")
-        assert ai.get_next_move() == (0, 0)
+        game = self.get_new_game([[" "," "," "],[" ","X"," "],[" "," "," "]], "O")
+        ai = self.get_ai_for_game(game, "O")
+        self.assert_next_move(ai, (0, 0))
 
     def test_given_first_go_with_player_not_in_center(self):
-        game = Game(Grid())
-        game.grid.grid = [
-            ["X"," "," "],
-            [" "," "," "],
-            [" "," "," "]]
-        game.player = "O"
-        ai = AI(game=game, player="O")
-        assert ai.get_next_move() == (1, 1)
+        game = self.get_new_game([["X"," "," "],[" "," "," "],[" "," "," "]], "O")
+        ai = self.get_ai_for_game(game, "O")
+        self.assert_next_move(ai, (1, 1))
